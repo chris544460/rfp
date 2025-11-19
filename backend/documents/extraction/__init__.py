8e8e977f3ee245770534b61dc785ad11f57d968b
chris544460 <christian.martinezcontreras@yale.edu>
2025-11-18 20:11:23 -0500

    Simplify extraction package exports

--- backend/documents/extraction/__init__.py
+++ backend/documents/extraction/__init__.py
@@ -1,6 +1,3 @@
 """Document-level question extraction helpers."""
 
-from .approved_qa_parser import ApprovedQAParser, QARecord, AnswerVariant
-from .question_extractor import QuestionExtractor
-
-__all__ = ["QuestionExtractor", "ApprovedQAParser", "QARecord", "AnswerVariant"]
+__all__ = ["QuestionExtractor"]
